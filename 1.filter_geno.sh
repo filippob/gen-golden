6ee3113fecc3eb6e076070e12763d29765dd6a68
filippob <filippo.biscarini@gmail.com>
2022-04-12 18:11:07 +0200

    script to filter genotypes

---
+++ 1.filter_geno.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## script to filter genotype data
+
+plink="$HOME/Downloads/plink"
+prj_folder="$HOME/Documents/ciampolini/ginevra_fiori/risultatiembarkdellanostracaninacloe"
+input_file="raw_data/31211050208214" ##transposed ped files
+output="Analysis/geno_filtered"
+
+## read data
+echo "processing file ${prj_folder}/${input_file}"
+$plink --dog --tfile "${prj_folder}/${input_file}" --geno 0.05 --mind 0.10 --chr 1-38 --bp-space 1 --snps-only --recode --out $output
+
+echo "DONE!"
+
