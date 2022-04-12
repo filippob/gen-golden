16756c7b23480e2d5717ef2079872bcfc4b06c8d
filippob <filippo.biscarini@gmail.com>
2022-04-12 18:55:16 +0200

    script for roh

--- 3.roh.sh
+++ 3.roh.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-## script to calculate inbreeding
+## script to calculate ROH
 
 plink="$HOME/Downloads/plink"
 prj_folder="$HOME/Documents/ciampolini/ginevra_fiori"
 input_file="Analysis/geno_filtered" ##transposed ped files
-output="Analysis/inbreeding"
+output="Analysis/roh"
 
 ## read data
 echo "processing file ${prj_folder}/${input_file}"
 ## --ibc not usable --> only one dog !!
-$plink --dog --file "${prj_folder}/${input_file}" --het small-sample --out $output
+$plink --dog --file "${prj_folder}/${input_file}" --homozyg  --homozyg-het 1 --out $output
 
 echo "DONE!"
 
